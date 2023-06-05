ff8e36f053d91c1e3ae81b0c23c7882bd0f84a8d
yehezkiel ermanto <yehezkiel.ermanto28@gmail.com>
2023-06-05 10:19:44 +0700

    add docker file

--- moeApp/settings.py
+++ moeApp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '03bmoc-8000.csb.app','moe-apps-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
